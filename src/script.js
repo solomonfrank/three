f08006b1950fec61c6e32803bad3c3a942c96139
@enwerro <rock.enwerem@tecnotree.com>
2022-10-05 13:12:49 +0100

    Add random donut

--- src/script.js
+++ src/script.js
@@ -212,6 +212,8 @@ const size = {
   height: window.innerHeight,
 };
 
+const textureLoader = new THREE.TextureLoader();
+const matcapTexture = textureLoader.load("/textures/matcaps/1.png");
 const fontLoader = new FontLoader();
 
 fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
@@ -227,25 +229,50 @@ fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
     bevelSize: 0.02,
   });
 
-  const textMaterial = new THREE.MeshBasicMaterial({ wireframe: true });
+  const textMaterial = new THREE.MeshMatcapMaterial();
+  textMaterial.matcap = matcapTexture;
   const textMesh = new THREE.Mesh(textGeometry, textMaterial);
 
-  textGeometry.computeBoundingBox();
-  textGeometry.translate(
-    -(textGeometry.boundingBox.max.x - 0.02) * 0.5,
-    -(textGeometry.boundingBox.max.y - 0.02) * 0.5,
-    -(textGeometry.boundingBox.max.z - 0.03) * 0.5
-  );
+  // textGeometry.computeBoundingBox();
+  // textGeometry.translate(
+  //   -(textGeometry.boundingBox.max.x - 0.02) * 0.5,
+  //   -(textGeometry.boundingBox.max.y - 0.02) * 0.5,
+  //   -(textGeometry.boundingBox.max.z - 0.03) * 0.5
+  // );
+
+  textGeometry.center();
   console.log(textGeometry.boundingBox);
 
   scene.add(textMesh);
+
+  console.time("donut");
+
+  for (let i = 0; i < 100; i++) {
+    const donutGeometry = new THREE.TorusBufferGeometry(0.35, 0.2, 20, 45);
+    const donutmaterial = new THREE.MeshMatcapMaterial();
+    donutmaterial.matcap = matcapTexture;
+    const donutMesh = new THREE.Mesh(donutGeometry, donutmaterial);
+    donutMesh.position.x = (Math.random() - 0.5) * 10;
+    donutMesh.position.y = (Math.random() - 0.5) * 10;
+    donutMesh.position.z = (Math.random() - 0.5) * 10;
+
+    donutMesh.rotation.x = Math.random() * Math.PI;
+    donutMesh.rotation.y = Math.random() * Math.PI;
+
+    const scale = Math.random();
+
+    donutMesh.scale.set(scale, scale, scale);
+    scene.add(donutMesh);
+  }
+
+  console.timeEnd("donut");
 });
 
 // axes helper
 
-const axesHelper = new THREE.AxesHelper();
+// const axesHelper = new THREE.AxesHelper();
 
-scene.add(axesHelper);
+// scene.add(axesHelper);
 
 // object creation
 // const geometry = new THREE.BoxGeometry(1, 1, 1);
