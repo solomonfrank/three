0e611da8378660e2027616c52206dcff45c0fb3f
@enwerro <rock.enwerem@tecnotree.com>
2022-10-20 17:20:09 +0100

    import model

--- src/script.js
+++ src/script.js
@@ -1,101 +1,83 @@
 import "./style.css";
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
 import * as dat from "dat.gui";
-import gsap from "gsap";
-
-/**
- * Debug
- */
-const gui = new dat.GUI();
-
-const parameters = {
-  materialColor: "#ffeded",
-};
-
-gui.addColor(parameters, "materialColor").onChange(() => {
-  material.color.set(parameters.materialColor);
-  particleMaterial.color.set(parameters.materialColor);
-});
 
 /**
  * Base
  */
+// Debug
+const gui = new dat.GUI();
+
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
 
 // Scene
 const scene = new THREE.Scene();
 
-//Texture;
+// model
 
-const textureLoader = new THREE.TextureLoader();
-const gradientTexture = textureLoader.load("/textures/gradients/3.jpg");
-gradientTexture.magFilter = THREE.NearestFilter;
-const darkTexture = textureLoader.load("/textures/gradients/5.jpg");
+// const dracoLoader = new DRACOLoader();
+const gltfLoader = new GLTFLoader();
+// gltfLoader.setDRACOLoader(dracoLoader);
+gltfLoader.load("/models/Fox/glTF/Fox.gltf", (gltf) => {
+  console.log("Loaded", gltf);
+  gltf.scene.scale.set(0.025, 0.025, 0.025);
+  scene.add(gltf.scene);
+
+  // const children = [...gltf.scene.children];
+
+  // for (let child of children) {
+  //   scene.add(child);
+  // }
+});
+// gltfLoader.load(
+//   "/models/FlightHelmet/glTF/FlightHelmet.gltf",
+//   (gltf) => {
+//     console.log(gltf);
+//     // while (gltf.scene.children.length) {
+//     //   console.log("Entered");
+//     //   scene.add(gltf.scene.children[0]);
+//     // }
+//   },
+//   () => {},
+//   (err) => {
+//     console.log(err);
+//   }
+// );
 
 /**
- * Objects
+ * Floor
  */
-
-const material = new THREE.MeshToonMaterial({
-  color: parameters.materialColor,
-  gradientMap: gradientTexture,
-});
-
-const objectDistance = 4;
-
-const mesh1 = new THREE.Mesh(new THREE.TorusGeometry(1, 0.4, 16, 60), material);
-
-const mesh2 = new THREE.Mesh(new THREE.ConeGeometry(1, 2, 32), material);
-const mesh3 = new THREE.Mesh(
-  new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16),
-  material
+const floor = new THREE.Mesh(
+  new THREE.PlaneGeometry(10, 10),
+  new THREE.MeshStandardMaterial({
+    color: "#444444",
+    metalness: 0,
+    roughness: 0.5,
+  })
 );
+floor.receiveShadow = true;
+floor.rotation.x = -Math.PI * 0.5;
+scene.add(floor);
 
-mesh1.position.y = -objectDistance * 0;
-mesh2.position.y = -objectDistance * 1;
-mesh3.position.y = -objectDistance * 2;
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
+scene.add(ambientLight);
 
-const meshes = [mesh1, mesh2, mesh3];
-
-scene.add(mesh1, mesh2, mesh3);
-
-// particles
-
-const count = 500;
-const positions = new Float32Array(count * 3);
-
-for (let i = 0; i < count; i++) {
-  positions[i * 3 + 0] = (Math.random() - 0.5) * 10;
-  positions[i * 3 + 1] =
-    objectDistance * 0.5 - Math.random() * objectDistance * 3;
-  positions[i * 3 + 2] = (Math.random() - 0.5) * 10;
-}
-
-console.log("positions", positions);
-
-const particleGeometry = new THREE.BufferGeometry();
-particleGeometry.setAttribute(
-  "position",
-  new THREE.BufferAttribute(positions, 3)
-);
-
-const particleMaterial = new THREE.PointsMaterial({
-  size: 0.03,
-  color: parameters.materialColor,
-  sizeAttenuation: true,
-});
-
-// points
-const points = new THREE.Points(particleGeometry, particleMaterial);
-scene.add(points);
-
-// light
-
-const directionalLight = new THREE.DirectionalLight("#ffffff", 1);
-
-directionalLight.position.set(1, 1, 0);
-
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6);
+directionalLight.castShadow = true;
+directionalLight.shadow.mapSize.set(1024, 1024);
+directionalLight.shadow.camera.far = 15;
+directionalLight.shadow.camera.left = -7;
+directionalLight.shadow.camera.top = 7;
+directionalLight.shadow.camera.right = 7;
+directionalLight.shadow.camera.bottom = -7;
+directionalLight.position.set(5, 5, 5);
 scene.add(directionalLight);
 
 /**
@@ -123,59 +105,31 @@ window.addEventListener("resize", () => {
 /**
  * Camera
  */
-
-const cameraGroup = new THREE.Group();
-scene.add(cameraGroup);
 // Base camera
 const camera = new THREE.PerspectiveCamera(
-  35,
+  75,
   sizes.width / sizes.height,
   0.1,
   100
 );
+camera.position.set(2, 2, 2);
+scene.add(camera);
 
-camera.position.z = 6;
-cameraGroup.add(camera);
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.target.set(0, 0.75, 0);
+controls.enableDamping = true;
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
-  alpha: true,
 });
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-
-let scrollY = window.scrollY;
-
-let currentSection = 0;
-
-window.addEventListener("scroll", (e) => {
-  scrollY = window.scrollY;
-
-  const newSection = Math.round(scrollY / sizes.height);
-  if (newSection != currentSection) {
-    currentSection = newSection;
-    gsap.to(meshes[currentSection].rotation, {
-      x: "+=6",
-      y: "+=3",
-      z: "+=1.5",
-      ease: "power1.out",
-      duration: 1.5,
-    });
-  }
-});
-
-const cursor = {
-  x: 0,
-  y: 0,
-};
-
-window.addEventListener("mousemove", (e) => {
-  cursor.x = e.clientX / sizes.width - 0.5;
-  cursor.y = e.clientY / sizes.height - 0.5;
-});
 
 /**
  * Animate
@@ -188,25 +142,8 @@ const tick = () => {
   const deltaTime = elapsedTime - previousTime;
   previousTime = elapsedTime;
 
-  // Animate camera;
-
-  camera.position.y = (-scrollY / sizes.height) * objectDistance;
-
-  mesh1.position.x = 2;
-  mesh2.position.x = -2;
-  mesh3.position.x = 2;
-
-  const parallaxX = cursor.x * 0.5;
-  const parallaxY = -cursor.y * 0.5;
-  cameraGroup.position.x +=
-    (parallaxX - cameraGroup.position.x) * 5 * deltaTime; // we are multipying by deltaTime to maintaine uniformity on both higher frame per sec device
-  cameraGroup.position.y +=
-    (parallaxY - cameraGroup.position.y) * 5 * deltaTime;
-
-  for (let mesh of meshes) {
-    mesh.rotation.x += deltaTime * 0.1;
-    mesh.rotation.y += deltaTime * 0.12;
-  }
+  // Update controls
+  controls.update();
 
   // Render
   renderer.render(scene, camera);
